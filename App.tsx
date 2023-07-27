8949a9a17a85af412111c866f29187f1948e5692
Robson Correia <robsoncorreia@192.168.100.4>
2023-07-27 16:35:51 -0300

    feat: add Loading component

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
-import { ActivityIndicator } from 'react-native';
 import { Groups } from '@screens/Groups';
 import { ThemeProvider } from 'styled-components'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import theme from '@theme/index';
+import { Loading } from '@components/Loading';
 
 export default function App() {
 
@@ -14,7 +14,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+      {!fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
