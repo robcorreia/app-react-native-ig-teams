94bc52d42feba8734706fb57cb93483ebb4e2adb
Robson Correia <robsoncorreia@192.168.100.4>
2023-07-27 16:02:16 -0300

    feat: add styled components + theme + types

--- App.tsx
+++ App.tsx
@@ -1,7 +1,12 @@
 import { Groups } from '@screens/Groups';
+import theme from '@theme/index';
+import { ThemeProvider } from 'styled-components'
 
 export default function App() {
+
   return (
-    <Groups />
+    <ThemeProvider theme={theme}>
+      <Groups />
+    </ThemeProvider>
   );
 }
