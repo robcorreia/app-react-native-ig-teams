4093b481f9e629be0c8fd42509d916c3e16f9e52
Robson Correia <robsoncorreia@192.168.100.4>
2023-07-28 11:45:20 -0300

    feat: add Header component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,11 @@
 import { Text } from 'react-native';
 import { Container } from './styles';
+import { Header } from '@components/Header';
 
 export function Groups() {
   return (
     <Container>
-
+      <Header />
     </Container>
   );
 }
