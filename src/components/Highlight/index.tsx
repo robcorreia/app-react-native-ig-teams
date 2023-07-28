6bfbc670d9c8d5aac7289f3e7af805ff56a4cdb3
Robson Correia <robsoncorreia@192.168.100.4>
2023-07-28 12:20:46 -0300

    feat: add Highlight component

---
+++ src/components/Highlight/index.tsx
@@ -0,0 +1,19 @@
+import { Container, Subtitle, Title } from "./styles";
+
+type HighlightProps = {
+  title: string;
+  subtitle: string;
+}
+
+export function Highlight({ title, subtitle }: HighlightProps) {
+
+  return (
+    <Container>
+
+      <Title>{title}</Title>
+
+      <Subtitle>{subtitle}</Subtitle>
+
+    </Container>
+  )
+}
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,13 @@
 import { Text } from 'react-native';
 import { Container } from './styles';
 import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
 
 export function Groups() {
   return (
     <Container>
       <Header />
+      <Highlight title='Turmas' subtitle='Jogue com a sua turma' />
     </Container>
   );
 }
