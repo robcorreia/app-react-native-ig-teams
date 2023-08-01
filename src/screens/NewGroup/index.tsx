7b05e96d7c22056ef46a9c15ff07005ad664d2d1
Robson Correia <robsoncorreia@192.168.100.4>
2023-08-01 20:05:19 -0300

    feat: add input Component

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -2,6 +2,7 @@ import { Header } from "@components/Header";
 import { Container, Content, Icon } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
+import { Input } from "@components/Input";
 
 export function NewGroup() {
 
@@ -16,8 +17,9 @@ export function NewGroup() {
           title="Nova turma"
           subtitle="crie a turma para adicionar as pessoas"
         />
-
-        <Button title="Criar" />
+        <Input
+          placeholder="Nome da turma" />
+        <Button style={{ marginTop: 20 }} title="Criar" />
       </Content>
 
     </Container>
