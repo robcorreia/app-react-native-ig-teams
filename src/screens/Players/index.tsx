8c8eec7fb4ce4003b39a3c8ca381c9377c24fce0
Robson <robccorreia@gmail.com>
2023-08-16 10:17:22 -0300

    feat: add Filter component

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -3,6 +3,7 @@ import { Container, Form } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from "@components/Input";
+import { Filter } from "@components/Filter";
 
 export function Players() {
 
@@ -24,6 +25,8 @@ export function Players() {
           icon='add'
         />
       </Form>
+
+      <Filter title="Time A" />
 
     </Container>
   )
