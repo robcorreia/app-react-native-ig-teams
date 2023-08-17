c0dac0413058d7b984faeb5af4811430681b239a
Robson <robccorreia@gmail.com>
2023-08-17 10:30:12 -0300

    feat: feat add navigation

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,21 @@
 import { GroupCard } from '@components/GroupCard';
-import { Container } from './styles';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { useState } from 'react';
 import { FlatList } from 'react-native';
 import { ListEmpty } from '@components/ListEmpty';
 import { Button } from '@components/Button';
+import { Container } from './styles';
+import { useNavigation } from '@react-navigation/native';
+
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([])
+  const navigation = useNavigation()
+
+  function handleNewGroup() {
+    navigation.navigate('new')
+  }
 
   return (
     <Container>
@@ -30,6 +37,7 @@ export function Groups() {
 
       <Button
         title="Criar nova turma"
+        onPress={handleNewGroup}
       />
 
     </Container>
